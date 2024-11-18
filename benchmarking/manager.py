939c333c9bbe8ddab1ec740da98111c79483fe9c
Brian Brogan <brian.brogan45@gmail.com>
2024-11-18 17:37:58 +0100

    Cleaned up, took visualization method from comparator.

--- benchmarking/manager.py
+++ benchmarking/manager.py
@@ -50,52 +50,9 @@ class DataManager:
         cont_series = cont_series.apply(
             lambda iseg_cont_list: [np.array(cont, dtype=np.int32) for cont in iseg_cont_list])
         return cont_series
-    
-    #shit
-    def get_single_marker_points(self,
-                          marker_array: np.ndarray, # single marker array
-                          target: int # target marker
-                          ):
-        target_points = np.where(marker_array == target)
-        #target_points = np.column_stack(np.where(marker_array == target))
-        return target_points
-    
-    #shit
-    def get_single_contour_points(self, 
-                           contour: np.ndarray # single contour
-                           ):
-        contour = contour.reshape((-1, 2))
-        min_x, min_y = np.min(contour, axis=0)
-        max_x, max_y = np.max(contour, axis=0)
-        grid_points = [(x, y) for x in range(min_x, max_x+1) for y in range(min_y, max_y+1)]
-        points_with_test_results = [pointPolygonTest(contour, pt, False) for pt in grid_points]
-        inside_points = [grid_points[i] for i, result in enumerate(points_with_test_results) if result >= 0]
-        return np.array(inside_points)
-    
-    def create_indicator_mask(self,
-                              #origin_img_fname: str,
-                              xrange: tuple = None,
-                              yrange: tuple = None,
-                              indicators: dict = None #key is a string, value is 1 or -1 to show positive or negative inficator
-                              ):
-        mask = np.zeros((xrange[1]-xrange[0], yrange[1]-yrange[0]), dtype=np.int8)
-        for ind, value in indicators.items():
-            sub_origin = self.origin_csv.iloc[self.origin_csv['geneID']==ind]
-            grabs_inds = (
-                (xrange[0] <= sub_origin['x']) & (sub_origin['x'] <= xrange[1]) &
-                (yrange[0] <= sub_origin['y']) & (sub_origin['y'] <= yrange[1])
-            )
-            grabs = sub_origin.loc[grabs_inds]
-            x_coords = (grabs['x'] - xrange[0]).astype(int).to_numpy()
-            y_coords = (grabs['y'] - yrange[0]).astype(int).to_numpy()
-
-            mask[y_coords, x_coords] = value
-
-        return mask
-        
-        
 
 
+    ## Visualizers ##
 
     def view_marker_array(self,
                           marker_array: np.ndarray
@@ -125,6 +82,21 @@ class DataManager:
         plt.title("Contour Visualization")
         plt.axis("off")
         plt.show()
+
+    def visualize_contours(self, 
+                           contours):
+        min_x, min_y = float('inf'), float('inf')
+        max_x, max_y = -float('inf'), -float('inf')
+        for i, contour in enumerate(contours):
+            x_vals, y_vals = zip(*[point[0] for point in contour]) 
+            min_x, min_y = min(min_x, min(x_vals)), min(min_y, min(y_vals))
+            max_x, max_y = max(max_x, max(x_vals)), max(max_y, max(y_vals))
+        image = np.zeros((max_y - min_y + 10, max_x - min_x + 10), dtype=np.uint8)
+        for contour in contours:
+            contour_shifted = np.array([[pt[0][0] - min_x, pt[0][1] - min_y] for pt in contour], dtype=np.int32)
+            drawContours(image, [contour_shifted], -1, 255, thickness=1)
+        plt.imshow(image, cmap='gray')
+        plt.title("Contours Visualization")
+        plt.axis('off')
+        plt.show()
     
-
-
